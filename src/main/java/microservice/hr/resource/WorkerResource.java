e415bad60a2cddd8cacd128c24d23db6754b7f1e
Rafael barro Gomes <rafael.gomes@supera.com.br>
2021-08-29 13:32:26 -0300

    Buscar por Id

--- src/main/java/microservice/hr/resource/WorkerResource.java
+++ src/main/java/microservice/hr/resource/WorkerResource.java
@@ -5,6 +5,7 @@ import microservice.hr.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker obj = workerRepository.findById(id).get();
+        return ResponseEntity.ok(obj);
     }
 }
